84164345a144edf1879ae36649dae1b43ac5b615
Muizz Rafid <muizzrafid1@gmail.com>
2024-02-24 15:05:54 +0600

    cg

---
+++ 06currencyConvertor/tailwind.config.js
@@ -0,0 +1,10 @@
+import { M } from "vite/dist/node/types.d-jgA8ss1A";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
