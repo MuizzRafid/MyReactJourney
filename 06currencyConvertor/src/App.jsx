7e1e5c534c9d8a43f980dcd2e7514285127a78ed
Muizz Rafid <muizzrafid1@gmail.com>
2024-02-24 20:04:13 +0600

    add

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1 className="text-4xl bg-orange-500">Currency App</h1>
